8c47d70428ec0c615bd4051b2a64788594d71218
ZiadAhmedShawky <ziadahmedshawky123@gmail.com>
2022-08-19 07:38:58 +0200

    I'm the best

--- public/js/app.js
+++ public/js/app.js
@@ -8,12 +8,12 @@ const msg2=document.querySelector('#message-2')
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location=search.value
-    
-    fetch('/weather?address='+location).then((response)=>{
+    msg2.textContent=" "
+    fetch('http://localhost:3000/weather?address='+location+'').then((response)=>{
     response.json().then((data)=>{
         if(data.error)
         {
-           msg1.textContent=data.error
+            return msg1.textContent=data.error
         }else{
             msg1.textContent=data.place
             msg2.textContent=data.forecast
--- src/utiles/forecast.js
+++ src/utiles/forecast.js
@@ -1,14 +1,14 @@
 const request=require('request')
 
 const forecast=(latitude,longitude,callback)=>{
-const url='http://api.weatherstack.com/current?access_key=9aef826362b037e95967d26628f972a5&query='+latitude+','+longitude+'&units=f'
+const url='http://api.weatherstack.com/current?access_key=9aef826362b037e95967d26628f972a5&query='+latitude+','+longitude+'&units=m'
     request({url,json:true},(error,{body})=>{
         if(error){
            callback('Unable to connect to internet',undefined);
         }else if(body.error){
             callback('can\'t find location',undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined,'The forecast today is '+body.current.temperature+' '+body.current.weather_descriptions[0])
         }
     })
 }
