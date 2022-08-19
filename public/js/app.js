53c423e04742cc6340ab9b9fba096574253b5138
ZiadAhmedShawky <ziadahmedshawky123@gmail.com>
2022-08-19 06:48:56 +0200

    modify

--- public/js/app.js
+++ public/js/app.js
@@ -8,16 +8,14 @@ const msg2=document.querySelector('#message-2')
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location=search.value
-
-    msg2.textContent=data.location
-
+    
     fetch('/weather?address='+location+'').then((response)=>{
     response.json().then((data)=>{
         if(!data.location)
         {
             return msg1.textContent=data.error
         }
-        
+        msg2.textContent=data.location
         msg2.textContent=data.weatherDescription;
     })
 })
