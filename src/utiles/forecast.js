40bf0913dbbad2c63b66fc4bdf025ddfa5208a8e
ZiadAhmedShawky <ziadahmedshawky123@gmail.com>
2022-08-21 11:35:02 +0200

    The forecast addition

--- src/utiles/forecast.js
+++ src/utiles/forecast.js
@@ -1,14 +1,15 @@
 const request=require('request')
 
 const forecast=(latitude,longitude,callback)=>{
-const url='http://api.weatherstack.com/current?access_key=9aef826362b037e95967d26628f972a5&query='+latitude+','+longitude+'&units=m'
+const url='http://api.weatherstack.com/current?access_key=4933c5157873609ba987a0b17a382934&query='+latitude+','+longitude+'&units=m'
     request({url,json:true},(error,{body})=>{
         if(error){
            callback('Unable to connect to internet',undefined);
         }else if(body.error){
+            console.log(body.error);
             callback('can\'t find location',undefined)
         }else{
-            callback(undefined,'The forecast today is '+body.current.temperature+' '+body.current.weather_descriptions[0]+', the humidity is '+body.current.humidity+' is the time now in that country is a day '+body.current.is_day)
+            callback(undefined,'The forecast today is '+body.current.temperature+' '+body.current.weather_descriptions[0]+', the humidity today is '+body.current.humidity+'%'+' and the current time is a day: '+body.current.is_day)
         }
     })
 }
