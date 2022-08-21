a1c3e2fd80cc723d968e99a3853fd578033a2b21
ZiadAhmedShawky <ziadahmedshawky123@gmail.com>
2022-08-21 11:05:42 +0200

    forecast changes

--- src/utiles/forecast.js
+++ src/utiles/forecast.js
@@ -8,7 +8,7 @@ const url='http://api.weatherstack.com/current?access_key=9aef826362b037e95967d2
         }else if(body.error){
             callback('can\'t find location',undefined)
         }else{
-            callback(undefined,'The forecast today is '+body.current.temperature+' '+body.current.weather_descriptions[0])
+            callback(undefined,'The forecast today is '+body.current.temperature+' '+body.current.weather_descriptions[0]+', the humidity is '+body.current.humidity+' is the time now in that country is a day '+body.current.is_day)
         }
     })
 }
